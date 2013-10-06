221620336d2456905abdb75be67b24d388d759e4
Max.Uhlenhuth <max@silviaterra.com>
2013-10-06 13:09:17 -0400

    finished decompress utils and testing

--- utils.py
+++ utils.py
@@ -1,18 +1,36 @@
+import os
+import shutil
 import tarfile
 import zipfile
 
-def decompress(fn):
+def decompress(filename, out_dir='/tmp/decompressed'):
     """
-    Given a tar.gz or a zip, extract the contents and return a list of files
+    Given a tar.gz or a zip, extract the contents and return a list of files.
+    The out_dir must not already exist.  It will be created from scratch and
+    filled with the files from the compressed file
+    """
+    fn = filename #alias
 
-    """
-    if zipfile.is_zipfile(fn):
-        pass
-    elif tarfile.is_tarfile(fn):
-        pass
-    else:
-        raise ValueError('Invalid file type - must be tar.gz or zip')
+    if os.path.exists(out_dir):
+        raise Exception('Output directory already exists')
+    os.makedirs(out_dir)
+    del_dir = False
+
+    try:
+        if zipfile.is_zipfile(fn):
+            zipfile.ZipFile(fn, 'r').extractall(out_dir)
+        elif tarfile.is_tarfile(fn):
+            tarfile.open(fn, 'r').extractall(out_dir)
+        else:
+            raise ValueError('Invalid file type - must be tar.gz or zip')
+    except Exception as e:
+        del_dir = True #delete the partially created out_dir
+        raise e #pass exception through
+    finally:
+        if del_dir:
+            shutil.rmtree(out_dir)
     
+    return [os.path.join(out_dir, fn) for fn in os.listdir(out_dir)]
 
 def rast2csv():
     pass
--- utils_test.py
+++ utils_test.py
@@ -1,11 +1,29 @@
 from nose.tools import raises
+import os
+import shutil
 import unittest
 
 import utils
 
 class UtilsTestCase(unittest.TestCase):
     
+    @raises(Exception)
+    def test_decompress_existing_dir(self):
+        utils.decompress('test_files/dummy.csv', '/tmp/')
+
     @raises(ValueError)
     def test_decompress_invalid_type(self):
-        utils.decompress('test_files/dummy.csv')
-        
+        utils.decompress('test_files/dummy.csv', '/tmp/test_invalid_type')
+    
+    def test_decompress_tar(self):
+        test_dir = '/tmp/test_tar'
+        files = utils.decompress('test_files/dummy.tar.gz', test_dir)
+        self.assertEquals(files, [os.path.join(test_dir, 'dummy.csv')])
+        shutil.rmtree(test_dir)
+
+    def test_decompress_zip(self):
+        test_dir = '/tmp/test_zip'
+        files = utils.decompress('test_files/dummy.zip', test_dir)
+        self.assertEquals(files, [os.path.join(test_dir, 'dummy.csv')])
+        shutil.rmtree(test_dir)
+
