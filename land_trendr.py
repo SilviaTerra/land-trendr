bd40be839a4513500a307d0f382bcc1cae498921
Max.Uhlenhuth <max@silviaterra.com>
2014-04-06 00:05:55 -0400

    cleaning up land_trendr file now that settings will be read from S3

--- land_trendr.py
+++ land_trendr.py
@@ -1,14 +1,15 @@
 import argparse
 import boto
-import re
 import tarfile
 
+import settings as s
 from mr_land_trendr_job import MRLandTrendrJob
 
 DEPENDENCIES_TARFILE = '/tmp/landtrendr_dependencies.tar.gz'
 DEPENDENCIES = [
+    'settings.py',
     'classes.py',
-    'utils.py',
+    'utils.py'
 ]
 
 DEFAULT_EMR_JOB_RUNNER_KWARGS = {
@@ -20,10 +21,6 @@ DEFAULT_EMR_JOB_RUNNER_KWARGS = {
     ],
     'python_archives': [DEPENDENCIES_TARFILE]
 }
-
-S3_REGEX = re.compile('s3://([\w\-]+)/([\w\-\./]+)')
-INPUT_FILE = 'input.txt'
-LOCAL_INPUT_FILE = '/tmp/%s' % INPUT_FILE
 
 
 def add_bootstrap_cmds():
@@ -45,78 +42,60 @@ def bundle_dependencies():
     tar.close()
 
 
-def create_input_file(platform, input_bucket, input_path):
-    connection = boto.connect_s3()
-    bucket = connection.get_bucket(input_bucket)
-
-    contents = '\n'.join(['%s\t%s' % (bucket.name, key.key) for key in bucket.list(prefix=input_path) if key.key.endswith('.zip') or key.key.endswith('.tar.gz')])
+def create_input_file(platform, job):
+    # just a dummy value.  All settings pulled from S3
+    contents = 'Running-%s' % job
 
     if platform == 'local':
-        o = open(LOCAL_INPUT_FILE, 'w')
+        local_input = '/tmp/input.txt'
+        o = open(local_input, 'w')
         o.write(contents)
         o.close()
 
-        return LOCAL_INPUT_FILE
+        return local_input
 
     if platform == 'emr':
-        input_key = '%s%s' % (input_path, INPUT_FILE)
-    
-        key = bucket.new_key(input_key)
+        connection = boto.connect_s3()
+        bucket = connection.get_bucket(s.S3_BUCKET)
+        key = bucket.new_key(s.IN_EMR_KEYNAME % job)
         key.set_contents_from_string(contents)
-    
-        return 's3://%s/%s' % (input_bucket, input_key)
+
+        return 's3://%s/%s' % (s.S3_BUCKET, key.key)
 
 
-def main(platform, input_bucket, input_path, index_eqn=None, output=None):
-    if not index_eqn:
-        index_eqn = 'B1'
-    
+def main(platform, job):
     args, job_runner_kwargs = [], {}
-    job_runner_kwargs['input_paths'] = [create_input_file(platform, input_bucket, input_path)]
+    job_runner_kwargs['input_paths'] = [create_input_file(platform, job)]
 
     if platform == 'emr':
-        args = ['-r', 'emr'] 
-        job_runner_kwargs['output_dir'] = output
+        args = ['-r', 'emr']
         add_bootstrap_cmds()
         emr_job_runner_kwargs = DEFAULT_EMR_JOB_RUNNER_KWARGS
         bundle_dependencies()
     else:
         emr_job_runner_kwargs = {}
 
-    job = MRLandTrendrJob(args=args, index_eqn=index_eqn, job_runner_kwargs=job_runner_kwargs, emr_job_runner_kwargs=emr_job_runner_kwargs)
+    job = MRLandTrendrJob(
+        args=args,
+        job_runner_kwargs=job_runner_kwargs,
+        emr_job_runner_kwargs=emr_job_runner_kwargs
+    )
 
     with job.make_runner() as runner:
         runner.run()
 
-        if platform == 'local':
+        if platform != 'emr':
             for line in runner.stream_output():
                 print job.parse_output_line(line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a LandTrendr job')
 
-    parser.add_argument('-p', '--platform', required=True, choices=['local', 'emr'], 
-        help='Which platform do you want to run on?')
-    parser.add_argument('-i', '--input', required=True,
-        help='Where to find the input.  s3 path.')
-    parser.add_argument('-o', '--output',
-        help='Where to save the output.  Only valid and required for EMR')
+    parser.add_argument('-p', '--platform', required=True,
+                        choices=['local', 'emr'],
+                        help='Which platform do you want to run on?')
+    parser.add_argument('-j', '--job', required=True,
+                        help='Which LandTrendr job do you want to run?')
 
     args = parser.parse_args()
-
-    if args.platform == 'emr':
-        if not args.output:
-            raise argparse.ArgumentError('Must specify output s3 path for EMR job')
-    elif args.platform == 'local':
-        if args.output:
-            raise argparse.ArgumentError('Output file not a local arg for local job')
-
-    match = S3_REGEX.match(args.input)
-
-    if match is None:
-        raise argparse.ArgumentError('Invalid input')
-
-    input_bucket, input_path = match.groups()
-
-    main(args.platform, input_bucket, input_path, output=args.output)
-
+    main(args.platform, args.job)
